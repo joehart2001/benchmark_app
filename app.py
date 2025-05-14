fecb4acf5582b6a975ebf8a682d6f922535f6bd3
joehart2001 <jh2536@cam.ac.uk>
2025-05-14 17:21:01 +0100

    50 nodes and rm gitignores

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
-
-import mlipx
-import pickle
 import os
-import zntrack
-from pathlib import Path
 import yaml
 
 # Specify the file path and mode for opening the file
@@ -12,10 +7,6 @@ with open("new_model_registry.yaml", "r") as file:
 models = list(data.keys()) #+ ["ref-data"]
 
 # RM GITIGNORES IN NODE FOLDERS: find nodes -name .gitignore -exec bash -c 'echo "!*" > "$0"' {} \;
-
-import dvc.api
-import json
-from mlipx.cli.main import load_node_objects, load_nodes_and_ref_node_lat
 
 
 from mlipx.cli.main import full_benchmark_compare
