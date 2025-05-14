d81b6eabe460eccfe4ea189cd70ee18a670bd13b
joehart2001 <jh2536@cam.ac.uk>
2025-05-14 11:37:12 +0100

    port

--- app.py
+++ app.py
@@ -28,5 +28,5 @@ app = full_benchmark_compare(
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8050))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=8050, debug=True)
 
